aaf81e46f033fbfb80ff2f1bbcf39c06864637a0
Frederico-Cassola-Dev <cfcassola@gmail.com>
2025-11-11 16:34:02 +0100

    Add installation files

---
+++ install-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install all packages in order
+
+./install-zsh.sh
+./install-ghostty.sh
+./install-stow.sh
+./install-dotfiles-omarchy.sh
+
+./set-shell.sh
---
+++ install-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install zsh
+ if ! command -v zsh &>/dev/null; then
+   yay -S --noconfirm --needed zsh
+ fi
+
